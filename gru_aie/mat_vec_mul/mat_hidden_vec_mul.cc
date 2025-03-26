ba7c9d7f8baa0392ce24711a3a5e7cbaf1cc26f8
Msapkas <michail.sapkas@gmail.com>
2025-03-27 00:02:41 +0100

    changed the initialization of the hidden state from inside the inf loop to once outside it

--- gru_aie/mat_vec_mul/mat_hidden_vec_mul.cc
+++ gru_aie/mat_vec_mul/mat_hidden_vec_mul.cc
@@ -10,27 +10,19 @@ void mat_hidden_vec_mul(input_stream<float> * __restrict in,
                         const float (&h_init)[H_VECTOR_SIZE]
 
 ){  
-    bool first_iteration_flag = true;
+    // bool first_iteration_flag = true;
     alignas(32) aie::accum<accfloat, VECTOR_LANES> acc;
     alignas(32) aie::vector<float, VECTOR_LANES> hidden[H_VECTOR_SIZE/VECTOR_LANES];
 
     alignas(32) aie::vector<float, VECTOR_LANES> * v_weights = (aie::vector<float, VECTOR_LANES>*) &weights;
     alignas(32) aie::vector<float, VECTOR_LANES> * v_hidden  = (aie::vector<float, VECTOR_LANES>*) &h_init;
 
+    for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
+        {
+        hidden[i] = v_hidden[i];
+    }
+
     for (;;){
-        chess_separator_scheduler();
-        if (first_iteration_flag) {
-            for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
-                {
-                hidden[i] = v_hidden[i];
-            }
-                first_iteration_flag = false;
-        } else {
-            for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
-                {
-                hidden[i] = readincr_v<4>(in);
-            }
-        }
         chess_separator_scheduler();
         // Compute
         for (int i = 0; i < DIST_COEFF; i++) chess_loop_count(DIST_COEFF)
@@ -44,6 +36,11 @@ void mat_hidden_vec_mul(input_stream<float> * __restrict in,
                                 );
             }
             writeincr(out, acc);
+        }
+        chess_separator_scheduler();
+        for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
+            {
+            hidden[i] = readincr_v<4>(in);
         }
         chess_separator_scheduler();
     }
--- gru_aie/new_hidden_state_kernel/new_hidden_state.cc
+++ gru_aie/new_hidden_state_kernel/new_hidden_state.cc
@@ -10,11 +10,37 @@ void new_hidden_state(  input_stream<float> * __restrict cand_hidden_state_in,
                         const float (&h_init)[H_VECTOR_SIZE]
 ){
     bool first_iteration_flag = true;
-    alignas(32) aie::vector<float, VECTOR_LANES> old_hidden_state[H_VECTOR_SIZE/VECTOR_LANES], hhat[H_VECTOR_SIZE/VECTOR_LANES], z[H_VECTOR_SIZE/VECTOR_LANES];
+    alignas(32) aie::vector<float, VECTOR_LANES> prev_hidden_state[H_VECTOR_SIZE/VECTOR_LANES], hhat[H_VECTOR_SIZE/VECTOR_LANES], z[H_VECTOR_SIZE/VECTOR_LANES];
     alignas(32) aie::accum<accfloat, VECTOR_LANES> new_hidden_state[H_VECTOR_SIZE/VECTOR_LANES];
 
     aie::vector<float, VECTOR_LANES> * v_hidden  = (aie::vector<float, VECTOR_LANES>*) &h_init;
 
+    // First iteration must use h_init
+    // Read Z and Cand Hidden State Gate from the aggregators
+    for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
+        { // readincr_v<4> will make sure we are reading float vectors of 4
+        z[i] = readincr_v<4>(z_in);
+        hhat[i] = readincr_v<4>(cand_hidden_state_in);
+    }
+
+    // Now perform (1-z)*prev_h + z*H_hat
+    for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
+    // enter pragma loop unroll
+        {
+        new_hidden_state[i] = aie::mul(aie::sub(float(1) , z[i]), v_hidden[i]);
+        new_hidden_state[i] = aie::mac(new_hidden_state[i], z[i], hhat[i]);
+        prev_hidden_state[i] = new_hidden_state[i].to_vector<float>(0); 
+        // Keep the new hidden state in memory for later, needs to be vector for the mac
+    }
+    chess_separator_scheduler();
+    // output new hidden state
+    for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
+        {
+        writeincr(new_hidden_state_out, prev_hidden_state[i]);
+        // The var is named prev_hidden_state but at this point is just the new_hidden_state in a vector form
+    }
+
+    // 
     for (;;){
         chess_separator_scheduler();
         // Data acquisition
@@ -23,31 +49,18 @@ void new_hidden_state(  input_stream<float> * __restrict cand_hidden_state_in,
             z[i] = readincr_v<4>(z_in);
             hhat[i] = readincr_v<4>(cand_hidden_state_in);
         }
-        chess_separator_scheduler();
-        if (first_iteration_flag) {
-            // Compute using h_init
-            for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
-            // enter pragma loop unroll
-                {
-                new_hidden_state[i] = aie::mul(aie::sub(float(1) , z[i]), v_hidden[i]);
+        // Compute using prev_hidden_state
+        for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
+            {
+                new_hidden_state[i] = aie::mul(aie::sub(float(1) , z[i]), prev_hidden_state[i]);
                 new_hidden_state[i] = aie::mac(new_hidden_state[i], z[i], hhat[i]);
-                old_hidden_state[i] = new_hidden_state[i].to_vector<float>(0); // Keep the new hidden state in memory for later, needs to be vector for the mac
-            }
-            first_iteration_flag = false;
-        } else {
-            // Compute using old_hidden_state
-            for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
-                {
-                new_hidden_state[i] = aie::mul(aie::sub(float(1) , z[i]), old_hidden_state[i]);
-                new_hidden_state[i] = aie::mac(new_hidden_state[i], z[i], hhat[i]);
-                old_hidden_state[i] = new_hidden_state[i].to_vector<float>(0); // Keep the new hidden state in memory for later needs to be vector for the mac
-            }
-        chess_separator_scheduler();
+                prev_hidden_state[i] = new_hidden_state[i].to_vector<float>(0); // Keep the new hidden state in memory for later needs to be vector for the mac
         }
+        chess_separator_scheduler();
         // output new hidden state
         for (int i = 0; i < H_VECTOR_SIZE/VECTOR_LANES; i++) chess_loop_count(H_VECTOR_SIZE/VECTOR_LANES)
             {
-            writeincr(new_hidden_state_out, old_hidden_state[i]);
+            writeincr(new_hidden_state_out, prev_hidden_state[i]);
         }
         chess_separator_scheduler();
     }
